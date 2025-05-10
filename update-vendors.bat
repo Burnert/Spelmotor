9d49300037c382d584451a2cf47e5d7682f1f242
Burnert <burnertski@gmail.com>
2025-05-10 18:41:11 +0200

    Added shaderc to update-vendors script

--- update-vendors.bat
+++ update-vendors.bat
@@ -1,4 +1,9 @@
-REM Odin Freetype ---------------------------------------------------
+REM Odin Freetype bindings ---------------------------------------------------
 
 git fetch odin-freetype main
 git subtree pull --prefix vendor/freetype odin-freetype main --squash
+
+REM Odin ShaderC bindings ----------------------------------------------------
+
+git fetch odin-shaderc master
+git subtree pull --prefix vendor/shaderc odin-shaderc master --squash
